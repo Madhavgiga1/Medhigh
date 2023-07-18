44d2ccdb8f86ba0e4f67cdf5259f8d540af34d86
Madhav <sharmamadhav208@gmail.com>
2023-07-18 23:57:35 +0530

    modified network result and adapter push

---
+++ app/src/main/java/com/example/piflish/NetworkResult.kt
@@ -0,0 +1,4 @@
+package com.example.piflish
+
+class NetworkResult {
+}
---
+++ app/src/main/java/com/example/piflish/adapter/contactadapter.kt
@@ -0,0 +1,10 @@
+package com.example.piflish.adapter
+
+import androidx.recyclerview.widget.RecyclerView
+
+class contactadapter:RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    class MyViewHolder:RecyclerView.ViewHolder(){
+
+    }
+}
